109c87b1ba4d9b8b99bace848c2a6eeaef660d55
Serenkiy <sergey.sherstobitov@gmail.com>
2026-07-13 11:11:45 +0300

    Poplavok bt tests

--- poplavok/app/www/js/ble.js
+++ poplavok/app/www/js/ble.js
@@ -1,23 +1,135 @@
 // UUID должны в точности соответствовать тем, что в ESP32
 
+if(window.cordova.platformId == "browser")
+	ble = ble_mock
 
-
-class BLE
+class BLEDevice
 {
+	constructor(device, wble)
+	{
+		this.wble = wble
+		for(let k in device)
+		{
+			this[k] = device[k]
+		}
 
+		this.is_connected = 0
+
+		this.reconnect_timeout = 0;
+	}
+
+	status(code, msg,level="info")
+	{
+		this.wble.status(code, {dev_id: this.id, msg}, level)
+	}
+
+	error(code, msg)
+	{
+		this.status(code, msg, "error")
+	}
+
+	// Подключение к ESP32
+	async connect(auto_reconnect=1) {
+		this.status("connect","Подключение к " + this.id + "...");
+		
+		return new Promise(s => ble.connect(
+			this.id, 
+			peripheral => {
+				this.status("connect_ok","Устройство успешно подключено")
+				this.is_connected = 1	
+				this.reconnect_timeout = 0
+				this.subscribeToData()
+				s(1)
+			}, 
+			peripheral => {
+				this.error("connect_error",'Сбой подключения: ' + JSON.stringify(err));
+				this.is_connected = 0;
+				if(auto_reconnect)
+					this.reconnect()
+				s(0)
+			}
+		))
+	}
+
+	async reconnect()
+	{
+		this.status("reconnect")
+		if(this.reconnect_timeout)
+		{
+			for(let i=this.reconnect_timeout;i > 0; i++)
+			{
+				this.status("reconnect_in",i)
+				await delay(1000)
+			}
+		}
+		
+		this.reconnect_timeout += 2
+
+		await this.connect(1)
+	}
+
+
+	async send(message, hint, as_text=0) {
+		this.status("send", hint)
+		if (!this.is_connected) {
+			this.error("send_no_connection", 'Нет активного подключения'); 
+			return 0;
+		}
+
+		if(!await this.wble.checkEnabled())
+			return 0
+
+		const buffer = as_text ? new TextEncoder().encode(message).buffer : message
+
+		return new Promise(s => ble.write(
+			this.id,
+			this.wble.SERVICE_UUID,
+			this.wble.CHAR_UUID,
+			buffer,
+			function() {
+				this.status("send_ok", 'Данные успешно отправлены');
+				s(1)
+			},
+			function(err) {
+				this.error("send_error",JSON.stringify(err));
+				s(0)
+			}
+		))
+	}
+
+	async subscribeToData() {
+		ble.startNotification(this.id, this.wble.SERVICE_UUID, this.wble.CHAR_UUID, 
+			function(buffer) {
+				// Этот коллбек вызывается КАЖДЫЙ РАЗ, когда устройство шлет данные
+				let dataString = bytesToString(buffer);
+				console.log("Получены новые данные: " + dataString);
+				
+				// Обновляем UI
+				this.status("received_data",dataString);
+			}, 
+			function(error) {
+				this.error("subscribe_error",error+"")
+			}
+		);
+	}
+}
+
+class WBLE
+{
 	SERVICE_UUID = "";
 	CHAR_UUID    = "";
 	DEVICE_NAME = "";
 	dev_id = 0
 	is_connected = 0
 	listeners = []
-	ble = null
+
+	devices = []
 
 	async init(device_name, svc_uuid_hex_array, char_uuid_hex_array)
 	{
-		this.ble = window.cordova.platformId == "browser" ? ble_mock : ble
 		try{
 			this.SERVICE_UUID = this.mkUUID(svc_uuid_hex_array)
+			clog("SERVICE_UUID: "+this.SERVICE_UUID)
 			this.CHAR_UUID = this.mkUUID(char_uuid_hex_array)
 			this.DEVICE_NAME = device_name
 
@@ -36,11 +148,7 @@ class BLE
 		}
 	}
 
-	async scanAndConnect()
-	{
-		if(await this.scan())
-			await this.connect()
-	}
+	
 
 	addListener(callback)
 	{
@@ -64,101 +172,81 @@ class BLE
 		{
 			strs.push(hex_array.slice(ind[0],ind[1]).toReversed().map(v => v.toString(16)).join(""))
 		}
-		return strs.toReversed().join("-")
+		return strs.toReversed().join("-").toLowerCase()
 	}
 
+	async checkEnabled()
+	{
+		if(await this.isEnabled())
+			return 1
+
+		this.error("not_enabled","Bluetooth не включён")
+	}
 
 	isEnabled()
 	{
-		return new Promise((s,j) => this.ble.isEnabled(() => s(1), () => s(0)))
+		return new Promise((s,j) => ble.isEnabled(() => s(1), () => s(0)))
 	}
 
 	enable()
 	{
-		return new Promise((s,j) => this.ble.enable(() => s(1), () => s(0)))
+		return new Promise((s,j) => ble.enable(() => s(1), () => s(0)))
 	}
 
 
-	async scan(timeout=10) {
+	async scan(timeout=10, stop_when_found=1) {
 		this.status("scan","Сканируем доступные устройства")
-		let found = 0
-		let to = new Promise((_,j) => setTimeout(j,timeout*1000))
+		let devices = []
+		let to = new Promise((_,j) => setTimeout(() => j("timeout"),timeout*1000))
 		let prm = new Promise((s,j) => {
-			this.ble.scan([], timeout, device => {
+			ble.scan([], timeout, device => {
 				// Логируем все найденные устройства для отладки
-				this.status("scan_device",'Найдено устройство: ' + (device.name || 'Без имени') + ' [' + device.id + ']');
-				
-				// Проверяем имя устройства, которое мы жестко прописали в коде ESP32
-				if (device.name === this.DEVICE_NAME) {
-					// console.log('Ура! ESP32 найден по имени. Останавливаем поиск и подключаемся...');
-					this.ble.stopScan();
-					this.dev_id_found = device.id
-					this.status("scan_device_found",`Устройство ${this.DEVICE_NAME} найдено!`)
-					s(1)
-					found = 1
+				this.status("scan_device_found",device);
+				if(device.name !== this.DEVICE_NAME)
+					return
+
+				if(this.devices.find(v => v.id == device.id))
+				{
+					this.status("scan_device_exists",device.id);
 				}
+				else
+				{
+					let dev = new BLEDevice(device, this)
+					devices.push(dev)
+					if(stop_when_found)
+						ble.stopScan();
+				}
+
 			}, function(err) {
-				this.error("scan_failure",err+"")
+				this.error("scan_error",err+"")
 				s(0)
 			})
 			
 		})
 
 		try{
-			return await Promise.race([to, prm])
+			await Promise.race([to, prm])
 		}catch(e)
 		{
-			this.error("scan_device_not_found",`Устройство ${this.DEVICE_NAME} не найдено`)
+			if(e == "timeout")
+			{
+				if(devices.length)
+				{
+					this.devices = [...this.devices, ...devices]
+					this.status("scan_completed",devices.map(d => d.id))
+					return 1
+				}
+				// this.error("scan_devices_not_found",`Устройство ${this.DEVICE_NAME} не найдено`)
+			}
+			else
+			{
+				this.error("scan_error",e+"")
+			}
+			
 			return 0
 		}
 	}
 
-	// Подключение к ESP32
-	async connect() {
-		this.status("connect","Подключение к " + this.dev_id + "...");
-		
-		return new Promise(s => this.ble.connect(
-			this.dev_id, 
-			peripheral => {
-				this.status("connect_ok","Устройство успешно подключено")
-				this.is_connected = 1	
-				s(1)
-			}, 
-			peripheral => {
-				this.error("connect_failure",'Сбой подключения: ' + JSON.stringify(err));
-				this.is_connected = 0;
-				s(0)
-				// setTimeout(function() {
-				// 	this.status("reconnect","Попытка переподключения к устройству")
-				// 	this.connect();
-				// }, 3000);
-			}
-		))
-	}
+	
 
-
-	async send(message, hint, as_text=0) {
-		this.status("send", hint)
-		if (!this.connectedDeviceId) {
-			this.error("send_no_connection", 'Нет активного подключения');
-			return 0;
-		}
-
-		const buffer = as_text ? new TextEncoder().encode(message).buffer : message
-
-		return new Promise(s => this.ble.write(
-			this.connectedDeviceId,
-			this.SERVICE_UUID,
-			this.CHAR_UUID,
-			buffer,
-			function() {
-				this.status("send_ok", 'Данные успешно отправлены');
-				s(1)
-			},
-			function(err) {
-				this.error("send_failure",JSON.stringify(err));
-				s(0)
-			}
-		))
-	}
 }
--- poplavok/app/www/js/main.js
+++ poplavok/app/www/js/main.js
@@ -3,20 +3,40 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onBLE(code, msg, level)
 {
 	let lvl = level=="error" ? 2 : 0
-	clog(`${level.toUpperCase()}: [${code}] ${msg}`, lvl)
+	clog(`${level.toUpperCase()}: [${code}] ${JSON.stringify(msg)}`, lvl)
 }
 
 async function onDeviceReady() {
-	clog('Cordova готова. Проверяем Bluetooth...');
-	
-	b = new BLE()
+	try{
+		clog('Cordova готова. Проверяем Bluetooth...');
+		
+		wble = new WBLE()
 
-	b.addListener(onBLE)
+		wble.addListener(onBLE)
 
-	if(await b.init(
-		"POPLAVOK",
-		[0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00],
-		[0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x01]
-	))
-		await b.scanAndConnect()
+		if(!await wble.init(
+			"POPLAVOK",
+			[0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00],
+			[0xFF, 0xEE, 0xDD, 0xCC, 0xBB, 0xAA, 0x99, 0x88, 0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x01]
+		))
+			throw "Cannot connect to Bluetooth"
+		
+		if(!await wble.scan())
+			throw "No devices scanned"
+		
+		let dev = wble.devices[0]
+		if(await dev.connect())
+		{
+			// for(let i = 0;;i++)
+			// {
+			// 	dev.send("Hello world! "+i,"hello",1)
+			// 	await delay(1000)
+			// }
+		}
+			
+
+	}catch(e)
+	{
+		cerror("Fatal error: "+e)
+	}
 }
